4a18039fd9d1010303522d0d32eef869c3ebe155
dGr8LookinSparky <hissparkyness@gmail.com>
2020-02-28 03:00:04 -0500

    assets: update the assets to v1.3.0-alpha.0.14.10

--- src/filesystem/fs_tremulous.cpp
+++ src/filesystem/fs_tremulous.cpp
@@ -47,8 +47,8 @@ struct core_resource_s {
 
 #define CORE_RESOURCES_1_3 \
     {0, "core_game_dll"},   /* Core dll defined by FS_CORE_GAME_DLL_NAME */ \
-    -498868165,             /* trem13/vms-gpp-v1.3.0-alpha.0.14.9.pk3 */ \
-    -754603633              /* trem13/data-v1.3.0-alpha.0.14.6.pk3 */ \
+    -885509249,             /* trem13/vms-gpp-v1.3.0-alpha.0.14.10.pk3 */ \
+    1335218602              /* trem13/data-v1.3.0-alpha.0.14.10.pk3 */ \
 
 #define CORE_RESOURCES_GPP \
     -1154612609,            /* gpp/vms-gpp1.pk3 */ \
